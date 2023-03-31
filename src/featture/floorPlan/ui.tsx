fb0d0db3b5355c57ed14b71a422a3f89c65a02a6
Andrew Posohov <fessysys911@gmail.com>
2023-03-31 13:19:49 +0300

    feat: add TileLayer

--- src/featture/floorPlan/ui.tsx
+++ src/featture/floorPlan/ui.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, GeoJSON } from "react-leaflet";
+import { MapContainer, GeoJSON, TileLayer } from "react-leaflet";
 import { GeoJsonObject } from "geojson";
 import dataElement from "./config/floor.json";
 import styles from "./floorPlan.module.scss";
@@ -10,8 +10,12 @@ export const FloorPlan: React.FC = () => {
       <MapContainer
         center={{ lat: 53.91687819154794, lng: 27.63435423374176 }}
         style={{ width: "100%", height: "100%" }}
-        zoom={19}
+        zoom={20}
       >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
         <GeoJSON data={dataElement as GeoJsonObject} />
       </MapContainer>
     </div>
